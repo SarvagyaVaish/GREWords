22f4d2fd05475af2a273e14d4b2aef2049fe1778
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-08-08 15:20:52 -0400

    refactored arrow key navigation code

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -1,30 +1,30 @@
 
 $(document).ready(function(){
 
-	var controller = ".lists-controller";
-	var wordName = controller + " #word-name";
-	var showHint = controller + " #show-hint";
-	var showMeaning = controller + " #show-meaning";
-	var wordMeaning = controller + " #word-meaning";
-	var wordSentence = controller + " #word-sentence";
+  var controller = ".lists-controller";
+  var wordName = controller + " #word-name";
+  var showHint = controller + " #show-hint";
+  var showMeaning = controller + " #show-meaning";
+  var wordMeaning = controller + " #word-meaning";
+  var wordSentence = controller + " #word-sentence";
 
-	$(showHint + '.test').show();
-	$(showHint + '.learn').hide();
-	$(showHint).css({'cursor': 'pointer'})
-	$(showHint).click(function() { 
-		$(wordSentence).show();
-		$(showHint).hide();
-	});
+  $(showHint + '.test').show();
+  $(showHint + '.learn').hide();
+  $(showHint).css({'cursor': 'pointer'})
+  $(showHint).click(function() { 
+    $(wordSentence).show();
+    $(showHint).hide();
+  });
 
 $(showMeaning + '.test').show();
 $(showMeaning + '.learn').hide();
 $(showMeaning).css({'cursor': 'pointer'})
-	$(showMeaning).click(function() {  
-		$(wordMeaning).show();
-		$(wordSentence).show();
-		$(showMeaning).hide();
-		$(showHint).hide();
-	});
+  $(showMeaning).click(function() {  
+    $(wordMeaning).show();
+    $(wordSentence).show();
+    $(showMeaning).hide();
+    $(showHint).hide();
+  });
 
 $(wordMeaning + '.test').hide();
 $(wordMeaning + '.learn').show();
@@ -33,15 +33,16 @@ $(wordSentence + '.test').hide();
 $(wordSentence + '.learn').show();
 
 function checkArrowKeys(e){
-	var arrs= [], key= window.event? event.keyCode: e.keyCode;
-	arrs[39]= 'right';
-	if(arrs[key]) {
-		window.location =  $("#next-btn").attr("href")
-	}
+  var arrs= [], key= window.event? event.keyCode: e.keyCode;
+  arrs[39]= '.right-arrow';
+  if(arrs[key]) {
+    window.location =  $(".respond-to-key-press" + arrs[key]).attr("href")
+  }
 }
-document.onkeydown=checkArrowKeys;
 
-
+if ($(".respond-to-key-press").length > 0) {
+  document.onkeydown=checkArrowKeys;
+}
 
 });
 
