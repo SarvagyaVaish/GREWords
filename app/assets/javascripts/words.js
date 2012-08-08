92ac70ab827559976fc6a957efad8eb2c5f90d87
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2012-08-08 15:54:25 -0400

    fixed word/show missing due to js error

--- app/assets/javascripts/words.js
+++ app/assets/javascripts/words.js
@@ -1,48 +1,17 @@
-
 $(document).ready(function(){
 
-  var controller = ".lists-controller";
+  var controller = ".words-controller";
   var wordName = controller + " #word-name";
   var showHint = controller + " #show-hint";
   var showMeaning = controller + " #show-meaning";
   var wordMeaning = controller + " #word-meaning";
   var wordSentence = controller + " #word-sentence";
 
-  $(showHint + '.test').show();
-  $(showHint + '.learn').hide();
-  $(showHint).css({'cursor': 'pointer'})
-  $(showHint).click(function() { 
-    $(wordSentence).show();
-    $(showHint).hide();
-  });
-
-$(showMeaning + '.test').show();
-$(showMeaning + '.learn').hide();
-$(showMeaning).css({'cursor': 'pointer'})
-  $(showMeaning).click(function() {  
-    $(wordMeaning).show();
-    $(wordSentence).show();
-    $(showMeaning).hide();
-    $(showHint).hide();
-  });
-
-$(wordMeaning + '.test').hide();
-$(wordMeaning + '.learn').show();
-
-$(wordSentence + '.test').hide();
-$(wordSentence + '.learn').show();
-
-function checkArrowKeys(e){
-  var arrs= [], key= window.event? event.keyCode: e.keyCode;
-  arrs[39]= '.right-arrow';
-  if(arrs[key]) {
-    window.location =  $(".respond-to-key-press" + arrs[key]).attr("href")
-  }
-}
-
-if ($(".respond-to-key-press").length > 0) {
-  document.onkeydown=checkArrowKeys;
-}
-
+  $(wordName).show();
+  $(showHint).hide();
+  $(showMeaning).hide();
+  $(wordMeaning).show();
+  $(wordSentence).show();
+  
 });
 
